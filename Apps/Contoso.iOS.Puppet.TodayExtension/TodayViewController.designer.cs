bae143796e6292cb49b5126ae3dbc001a412ec88
Jacob Wallraff <jawal@microsoft.com>
2019-08-15 15:01:24 -0700

    Adding extension spike code to repo

---
+++ Apps/Contoso.iOS.Puppet.TodayExtension/TodayViewController.designer.cs
@@ -0,0 +1,32 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Contoso.iOS.Puppet.TodayExtension
+{
+    [Register ("TodayViewController")]
+    partial class TodayViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel randoLabel { get; set; }
+
+        [Action ("UIButton205_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton205_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (randoLabel != null) {
+                randoLabel.Dispose ();
+                randoLabel = null;
+            }
+        }
+    }
+}
